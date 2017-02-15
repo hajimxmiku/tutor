c87bf695d64883712b69a0f4692e4f188fcca56f
hajimxmiku <anakhandsomee@gmail.com>
2017-02-15 15:19:19 +0800

    Update Increase-Open-Files-Limit.sh

--- Increase-Open-Files-Limit.sh
+++ Increase-Open-Files-Limit.sh
@@ -1,9 +1,13 @@
-#Increasing Open Files Limit For Pritunl on debian 8
+#!/bin/bash
 
+# go to root
+cd
+
+#Increasing Open Files Limit For Pritunl on debian 8
 sudo sh -c 'echo "* hard nofile 64000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "* soft nofile 64000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "root hard nofile 64000" >> /etc/security/limits.conf'
-sudo sh -c 'echo "root soft nofile 64000" >> /etc/security/limits.conf
+sudo sh -c 'echo "root soft nofile 64000" >> /etc/security/limits.conf'
 
 #info
 echo -e "\e[34m THIS HOPEFULLY WILL FIX SOME ISSUE"
