6cc4575894b4e324d073313c9388787e4c03eefd
hajimxmiku <anakhandsomee@gmail.com>
2017-02-15 12:28:54 +0800

    Add files via upload

---
+++ Increase-Open-Files-Limit.sh
@@ -0,0 +1,6 @@
+#Increasing Open Files Limit For Pritunl on debian 8
+
+sudo sh -c 'echo "* hard nofile 64000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "* soft nofile 64000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "root hard nofile 64000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "root soft nofile 64000" >> /etc/security/limits.conf
