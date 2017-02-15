77f7268b385f2d1452b32bde5678b01be1c9e3f0
hajimxmiku <anakhandsomee@gmail.com>
2017-02-15 12:57:10 +0800

    Update Increase-Open-Files-Limit.sh

--- Increase-Open-Files-Limit.sh
+++ Increase-Open-Files-Limit.sh
@@ -4,3 +4,6 @@ sudo sh -c 'echo "* hard nofile 64000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "* soft nofile 64000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "root hard nofile 64000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "root soft nofile 64000" >> /etc/security/limits.conf
+
+#info
+echo -e "\e[34m THIS HOPEFULLY WILL FIX SOME ISSUE"
